c1460469dd5873f86017ac147338147152ccb52a
EduVF <eduardvf@outlook.com>
2023-01-18 00:16:17 +0100

    add parser

--- docs/assets/js/joc.mjs
+++ docs/assets/js/joc.mjs
@@ -60,22 +60,28 @@ export function lex(s) {
             }
             tok.push({ type: 's', val: s.slice(from, i), line: line });
             i++; // ignore ending quote
-        } else if (/[a-z_]/.test(char)) {
+        } else if (/[a-z^_]/.test(char)) {
             // word keyword
             let from = i;
-            for (i++; i < s.length && /[\w!?~+]/.test(s[i]); i++);
-            tok.push({ type: 'k', val: s.slice(from, i), line: line });
+            for (i++; i < s.length && /[\w^!?~+]/.test(s[i]); i++);
+            tok.push({ type: 'w', val: s.slice(from, i), line: line });
         } else if (/[!-~]/.test(char)) {
             // number or symbol keyword
             let from = i;
-            for (i++; i < s.length && '!"#$%&*+-./0123456789:;<=>?@\\^`|~'.includes(s[i]); i++);
+            for (i++; i < s.length && /[!-~]/.test(s[i]) && /[^'(),\[\]{}]/.test(s[i]); i++);
             let t = s.slice(from, i);
             // check if number (int, float, ratio) or keyword
             if (/^-?\d+$/.test(t)) {
+                // integer
+                tok.push({ type: 'i', val: Number(t), line: line });
+            } else if (/^0[xb]\d+$/.test(t)) {
+                // hex or binary (same as integer)
                 tok.push({ type: 'i', val: Number(t), line: line });
             } else if (/^-?(\d*\.\d+)|(\d+\.\d*)$/.test(t)) {
+                // float
                 tok.push({ type: 'f', val: Number(t), line: line });
             } else if (/^-?\d+:-?\d+$/.test(t)) {
+                // ratio
                 let ratio = t.split(':').map((e) => Number(e));
                 tok.push({ type: 'r', val: ratio, line: line });
             } else {
@@ -90,6 +96,46 @@ export function lex(s) {
         }
     }
     return tok;
+}
+
+/**
+ * Take a list of tokens and return an AST
+ * @param {Array.<object>} tok
+ */
+export function parse(tok, head = true) {
+    while (tok.length > 0) {
+        let t = tok.shift();
+        // an expression can start with...
+        if (t.type === 'k' || (head && t.type === 'w')) {
+            // - a symbol keyword (k)
+            // - a word keyword (w), given that head == true
+            let expr = [t];
+            while (tok.length > 0 && !'\n)'.includes(tok[0].type)) {
+                expr.push(parse(tok, false));
+            }
+            return expr;
+        } else if (t.type === '(') {
+            // - a parenthesis (...)
+            let expr = [];
+            let isMulti = tok[0].type === '\n';
+            // a new line after the opening parenthesis treats all word keywords
+            // starting a line, as starting an expression
+            while (tok.length > 0 && tok[0].type !== ')') {
+                expr.push(parse(tok, isMulti));
+                while (tok.length > 0 && tok[0].type === '\n') tok.shift();
+            }
+            if (tok.length === 0) {
+                msg.push(`[*] Missing ending parenthesis for expression starting at line ${t.line}.`);
+            } else {
+                tok.shift(); // remove ')'
+            }
+            return expr;
+        } else if (t.type === '\n') {
+            continue; // ignore extra new lines
+        }
+        return t;
+    }
+    return [];
 }
 
 export function evaluate(node, env) {
