244c7bbb95d4408c30564841cdc57edfcdb3413e
EduVF <eduardvf@outlook.com>
2023-01-18 23:56:00 +0100

    refactor basic lib

--- docs/assets/js/lib.mjs
+++ docs/assets/js/lib.mjs
@@ -1,26 +1,47 @@
+//@ts-check
+
+/**
+ * file: lib.mjs
+ * repo: github.com/eduvf/joc
+ *
+ * Each function in this module returns an object with the
+ * contents of its library.
+ */
+
+//--------------------------------------------------------------
+
+/**
+ * Standard library
+ */
 export function std(evaluate) {
-    return {
-        '+': {
+    function op(binFunc, unFunc = (x) => x) {
+        return {
             type: 'function',
             value: (arg, env, line, msg) => {
-                if (arg.length > 0) {
-                    let first = evaluate(arg[0], env);
-                    let type = first.type;
-                    arg.shift();
-                    return {
-                        type: type,
-                        value: arg.reduce((prev, curr) => {
-                            curr = evaluate(curr, env);
-                            if (curr.type !== type) {
-                                msg.push(`[*] Type mismatch in function at line ${line}.`);
-                                msg.push(`    Expected ${type}, got ${curr.type}. `);
-                            }
-                            return prev + curr.value;
-                        }, first.value),
-                    };
-                }
-                return { type: 'nothing' };
+                if (arg.length === 0) return { type: 'nothing' };
+                if (arg.length === 1) return unFunc(evaluate(arg[0], env).value);
+                let first = evaluate(arg[0], env);
+                const type = first.type;
+                return arg.slice(1).reduce((total, e) => {
+                    e = evaluate(e, env);
+                    if (e.type !== type) {
+                        msg.push(`[*] Type mismatch in function at line ${line}.`);
+                        msg.push(`    Expected ${type}, got ${e.type}. `);
+                    }
+                    return { type: type, value: binFunc(total, e.value) };
+                }, first.value);
             },
-        },
+        };
+    }
+
+    return {
+        '+': op((x, y) => x + y),
+        '-': op(
+            (x, y) => x - y,
+            (x) => -x
+        ),
+        '*': op((x, y) => x * y),
+        '/': op((x, y) => x / y),
+        '%': op((x, y) => x % y),
     };
 }
--- docs/assets/js/test.mjs
+++ docs/assets/js/test.mjs
@@ -2,8 +2,7 @@ import { lex, parse, evaluate, show } from './joc.mjs';
 import { std } from './lib.mjs';
 
 let s1 = `
-+ 1. (
-    + 2. 3)
++ 1 - 2 * 3 / 4 5
 `;
 
 console.log(show(evaluate(parse(lex('(' + s1 + ')')), [std(evaluate)])));
