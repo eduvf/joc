9dd5637a77e910aadaacd4b9973616f281a76c86
EduVF <eduardvf@outlook.com>
2023-01-21 22:02:53 +0100

    functions let, set, random and reshape

--- docs/assets/js/lib.mjs
+++ docs/assets/js/lib.mjs
@@ -17,21 +17,92 @@
  * @returns {Array.<Object>}
  */
 export function lib(interpret, log) {
+    function op(fn) {
+        return (arg, env, line) => {
+            arg = arg.map((a) => interpret(a, env));
+            let type = typeof arg.at(0);
+            return arg.reduce((prev, curr) => {
+                if (typeof curr !== type) log(`[*] Type mismatch at line ${line}`);
+                return fn(prev, curr);
+            });
+        };
+    }
+
     const std = {
+        // (arg, env, line) => {},
+
         // print
         ';': (arg, env, line) => {
             arg = arg.map((a) => interpret(a, env));
             arg.map(log);
             return arg;
         },
+        // let
+        ':': (arg, env, line) => {
+            let r = undefined;
+            while (arg.length > 0) {
+                let name = arg.shift();
+                let value = arg.shift();
+                if (name.type === 'word') {
+                    r = value ? interpret(value, env) : undefined;
+                    env[env.length - 1][name.value] = r;
+                    continue;
+                }
+                log(`[*] Invalid name for assignment at line ${line}`);
+            }
+            return r;
+        },
+        // set
+        '.': (arg, env, line) => {
+            let r = undefined;
+            while (arg.length > 0) {
+                let name = arg.shift();
+                let value = arg.shift();
+                if (name.type === 'word' && name.value.charAt(0) !== '_') {
+                    r = value ? interpret(value, env) : undefined;
+                    for (let i = env.length - 1; i >= 0; i--) {
+                        if (name.value in env[i]) {
+                            env[i][name.value] = r;
+                            break;
+                        }
+                    }
+                    continue;
+                }
+                log(`[*] Invalid name for assignment at line ${line}`);
+            }
+            return r;
+        },
         // add
-        '+': (arg, env, line) => {
-            arg = arg.map((a) => interpret(a, env));
-            let type = typeof arg.at(0);
-            return arg.reduce((prev, curr) => {
-                if (typeof curr !== type) log(`[*] Type mismatch at line ${line}`);
-                return prev + curr;
-            }, 0);
+        '+': op((x, y) => x + y),
+        // mul
+        '*': op((x, y) => x * y),
+        // random
+        '??': (arg, env, line) => {
+            if (arg.length === 0) return Math.random();
+            return interpret(arg[Math.floor(Math.random() * arg.length)], env);
+        },
+        // reshape
+        '<>': (arg, env, line) => {
+            let axis = [];
+            while (arg.length > 1) {
+                let n = interpret(arg.shift(), env);
+                if (typeof n !== 'number' || n < 0) {
+                    log(`[!] <> requires numbers >= 0 as arguments before the table`);
+                    return [];
+                }
+                axis.push(n);
+            }
+            let tab = Array(axis.reduce((p, c) => p * c))
+                .fill(0)
+                .map(() => interpret(arg[0], env));
+            for (let n of axis) {
+                let temp = [];
+                while (tab.length > 0) {
+                    temp.push(tab.splice(0, n));
+                }
+                tab = temp;
+            }
+            return tab;
         },
     };
     return [std];
--- docs/assets/js/test.mjs
+++ docs/assets/js/test.mjs
@@ -1,43 +1,30 @@
 import { joc } from './joc.mjs';
 
 let s1 = `
-; + 1 2 3
-; 'hi'
-; [1 2 3]
-; [
-    [1 2 3]
-    [4 5 6]
-    [7 8 9]
-]
-; [
-    [
-        [1 1 1]
-        [2 2 2]
-        [3 3 3]
-    ]
-    [
-        [4 4 4]
-        [5 5 5]
-        [6 6 6]
-    ]
-    [
-        [7 7 7]
-        [8 8 8]
-        [9 9 9]
-    ]
-]
+: x 2 y 3 z 4
+: curr <> x y z ?? 0 1
 `;
 
 let s2 = `
+: w 5 h 3
+; : curr <> w h ?? 0 1
+,; . curr <|> -1 0 1 curr
+,; . curr <-> -1 0 1 curr
+,; . curr + curr
+,; . curr =| 3 4 curr
+'done'
+`;
+
+let s3 = `
 : w 64 h 32
 : curr <> w h ?? 0 1
 : next ~ c =| 3 4 + <-> -1 0 1 <|> -1 0 1 c
 win w h (
     bg 0
-    @# x y curr ? ^ pix x y 0xfff
+    @# y x curr ? ^ pix x y 0xfff
     . curr (next curr)
     zzz .1
 )
 `;
 
-joc('(' + s1 + ')');
+joc('(' + s2 + ')');
