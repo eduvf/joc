29e403bd163256c75204c297f83556632a2c4d95
EduVF <eduardvf@outlook.com>
2023-01-22 20:14:52 +0100

    readd shape

--- docs/assets/js/lib.mjs
+++ docs/assets/js/lib.mjs
@@ -33,12 +33,37 @@ export function lib(interpret, log) {
 
     const op = (bin, un) => (arg, env, line) => {
         arg = arg.map((a) => interpret(a, env));
-        if (arg.length === 1) return un ? un(arg[0]) : arg[0].reduce((p, c) => bin(p, c));
+        if (arg.length === 1) {
+            if (!Array.isArray(arg[0])) return un(arg[0]);
+            arg = arg[0];
+        }
         let type = typeof arg.at(0);
         return arg.reduce((p, c) => {
             if (typeof c !== type) log(`[*] Type mismatch at line ${line}`);
             return bin(p, c);
         });
+    };
+
+    const compare = (bin) => (arg, env, line) => {
+        let p = arg.at(0) ? interpret(arg[0], env) : null;
+        let r = p ? 1 : 0;
+        for (let c of arg.slice(1)) {
+            c = interpret(c, env);
+            r = bin(p, c) ? 1 : 0;
+            p = c;
+            if (r === 0) break;
+        }
+        return r;
+    };
+
+    const shape = (name, fn) => (arg, env, line) => {
+        let last = arg.at(-1);
+        let axis = arg.slice(0, -1).map((a) => {
+            a = interpret(a, env);
+            if (typeof a === 'number' || a >= 0) return a;
+            log(`[!] Function '${name}' at line ${line} requires numbers >= 0 as axis`);
+        });
+        return fn(axis, last, env);
     };
 
     const std = {
@@ -101,54 +126,49 @@ export function lib(interpret, log) {
         '/': op((x, y) => x / y),
         // mod
         '%': op((x, y) => x % y),
+        // not
+        // and
+        // or
         // eq
-        '=': op((x, y) => x === y),
+        '=': compare((x, y) => x === y),
         // lt
-        '<': op((x, y) => x < y),
+        '<': compare((x, y) => x < y),
         // gt
-        '>': op((x, y) => x > y),
+        '>': compare((x, y) => x > y),
         // neq
-        '!=': op((x, y) => x !== y),
+        '!=': compare((x, y) => x !== y),
         // lte
-        '<=': op((x, y) => x <= y),
+        '<=': compare((x, y) => x <= y),
         // gte
-        '>=': op((x, y) => x >= y),
+        '>=': compare((x, y) => x >= y),
         // random
         '??': (arg, env, line) => {
             if (arg.length === 0) return Math.random();
             return interpret(arg[Math.floor(Math.random() * arg.length)], env);
         },
+        // reshape
+        '<>': shape('<>', (axis, last, env) => {
+            let tab = Array(axis.reduce((p, c) => p * c))
+                .fill(0)
+                .map(() => interpret(last, env));
+            for (let a of axis) {
+                let temp = [];
+                while (tab.length > 0) temp.push(tab.splice(0, a));
+                tab = temp;
+            }
+            return { $: axis, _: tab[0] };
+        }),
+        // reverse
+        '<->': shape('<->', (axis, last, env) => {
+            let tab = interpret(last, env);
+            // TODO
+        }),
     };
     return [std];
 }
 
 /*
 export function lib(interpret, log) {
-    const pairs = (fn) => (arg, env, line) => {
-        let r = undefined;
-        for (let i = 0; i < arg.length; i += 2) {
-            let key = arg.at(i);
-            let val = arg.at(i + 1);
-            if (key.type === 'word') {
-                r = val ? interpret(val, env) : undefined;
-                fn(key.value, r, env, line);
-                continue;
-            }
-            log(`[*] Invalid name for assignment at line ${line}`);
-        }
-        return r;
-    };
-
-    const op = (bin, un) => (arg, env, line) => {
-        arg = arg.map((a) => interpret(a, env));
-        if (arg.length === 1) return un ? un(arg[0]) : arg[0].reduce((p, c) => fn(p, c));
-        let type = typeof arg.at(0);
-        return arg.reduce((p, c) => {
-            if (typeof c !== type) log(`[*] Type mismatch at line ${line}`);
-            return bin(p, c);
-        });
-    };
-
     const shape = (name, fn) => (arg, env, line) => {
         let last = arg.at(-1);
         let axis = arg.slice(0, -1).map((a) => {
